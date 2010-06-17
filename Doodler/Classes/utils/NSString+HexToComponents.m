49d0eee5e8e0a66650e83f44e7620637d4156009
Sam Dean <sam@oneblacksock.co.uk>
2010-06-17 01:29:55 +0100

    Forgot the utils folder

--- Doodler/Classes/utils/NSString+HexToComponents.m
+++ Doodler/Classes/utils/NSString+HexToComponents.m
@@ -12,31 +12,31 @@
 @implementation NSString (HexToComponents)
 
 - (void)hexToCharComponents:(unsigned char *)charArray {
-	// Get the int value
-	NSScanner *scanner = [NSScanner scannerWithString:self];
-	unsigned intValue = 0x000000;
-	if (NO == [scanner scanHexInt:&intValue]) {
-		for (int n = 0; n < 4; ++n)
-			*charArray++ = 1.0f;
-		return;
-	}
-	
-	// Put the components into the array of floats passed in
-	*charArray++ = (intValue >> 16) & 0xff;
-	*charArray++ = (intValue >> 8) & 0xff;
-	*charArray++ = (intValue >> 0) & 0xff;
-	*charArray = 0xff;
+  // Get the int value
+  NSScanner *scanner = [NSScanner scannerWithString:self];
+  unsigned intValue = 0x000000;
+  if (NO == [scanner scanHexInt:&intValue]) {
+    for (int n = 0; n < 4; ++n)
+      *charArray++ = 1.0f;
+    return;
+  }
+
+  // Put the components into the array of floats passed in
+  *charArray++ = (intValue >> 16) & 0xff;
+  *charArray++ = (intValue >> 8) & 0xff;
+  *charArray++ = (intValue >> 0) & 0xff;
+  *charArray = 0xff;
 }
 
 - (void)hexToFloatComponents:(CGFloat *)floatArray {
-	unsigned char charArray[] = {0,0,0,0};
-	
-	// Get the integer components
-	[self hexToCharComponents:charArray];
-	
-	// Convert them to floats
-	for (int n = 0; n < 4; ++n)
-		*floatArray++ = (CGFloat)(charArray[n]) / 255.0;
+  unsigned char charArray[] = {0,0,0,0};
+
+  // Get the integer components
+  [self hexToCharComponents:charArray];
+
+  // Convert them to floats
+  for (int n = 0; n < 4; ++n)
+    *floatArray++ = (CGFloat)(charArray[n]) / 255.0;
 }
 
 @end
--- Doodler/Classes/utils/UIColor+ColorWithHexString.m
+++ Doodler/Classes/utils/UIColor+ColorWithHexString.m
@@ -13,12 +13,12 @@
 @implementation UIColor (ColorWithHexString)
 
 + (id) colorWithHexString:(NSString *)hexString {
-	CGFloat colors[4] = {1.0,1.0,1.0,1.0};
-	
-	[hexString hexToFloatComponents:colors];
-	
-	// Return the color
-	return [UIColor colorWithRed:colors[0] green:colors[1] blue:colors[2] alpha:colors[3]];
+  CGFloat colors[4] = {1.0,1.0,1.0,1.0};
+
+  [hexString hexToFloatComponents:colors];
+
+  // Return the color
+  return [UIColor colorWithRed:colors[0] green:colors[1] blue:colors[2] alpha:colors[3]];
 }
 
 @end
